0e2b7eb826530de149810c78e91bc29aac866f05
Kleea <kleea@students.uni-marburg.de>
2020-04-18 16:24:17 +0200

    fixed a few bugs in shader

--- UnityStuff/Assets/Scripts/tests/TestSuite.cs
+++ UnityStuff/Assets/Scripts/tests/TestSuite.cs
@@ -29,16 +29,18 @@ namespace FoPra.tests
 
             for (int i = 0; i < sizes.Length; i++)
             {
+               
+
                 Stopwatch stopWatch = new Stopwatch();
                 stopWatch.Start();
                 var res = execute_Distances2D(computeShader, sizes[i]);
                 stopWatch.Stop();
                 TimeSpan ts = stopWatch.Elapsed;
-                
+
                 string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
                     ts.Hours, ts.Minutes, ts.Seconds,
                     ts.Milliseconds / 10);
-                Debug.Log($"RunTime (n={sizes[i]}, n^2={sizes[i]*sizes[i]}):\t{elapsedTime}");
+                Debug.Log($"RunTime (n={sizes[i]}, n^2={sizes[i] * sizes[i]}):\t{elapsedTime}");
             }
         }
 
@@ -46,12 +48,12 @@ namespace FoPra.tests
         public static Vector2[] execute_Distances2D(ComputeShader cs, int size)
         {
             /* Data parameters */
-            var r_cell = 1.01f;
-            var r_sample = 1.0f;
-            var scale_factor = 1000.0f; // (float) Math.Pow(2, 10);
+            int scale_factor = 100; // (float) Math.Pow(2, 10);
+            var r_cell = 1.01f*scale_factor;
+            var r_sample = 1.0f*scale_factor;
             var margin = 1.05f;
             var theta = 25.0;
-            var (a, b) = (-r_sample*scale_factor*margin, r_sample*scale_factor*margin);
+            var (a, b) = (-r_sample*margin, r_sample*margin);
             
             /* Data generation */
             var data = Enumerable.Range(0, size)
@@ -65,36 +67,44 @@ namespace FoPra.tests
             var res = new Vector2[size*size];
             Array.Clear(res,0,size*size);
 
+
             /* Pass data to shader */
             int kernelHandle = cs.FindKernel("g1_dists");
             var inputBuffer = new ComputeBuffer(data.Length, 8);
-			var outputBufferOuter = new ComputeBuffer(data.Length, 8);
+            var outputBufferOuter = new ComputeBuffer(data.Length, 8);
             var outputBufferInner = new ComputeBuffer(data.Length, 8);
             cs.SetBuffer(kernelHandle, "segment", inputBuffer);
             cs.SetBuffer(kernelHandle, "distancesInner", outputBufferInner);
-			cs.SetBuffer(kernelHandle, "distancesOuter", outputBufferOuter);
-            cs.SetFloat("r_cell", r_cell*scale_factor);
-            cs.SetFloat("r_sample", r_sample*scale_factor);
-            cs.SetFloat("r_cell_sq", (float) Math.Pow(r_cell, 2)*scale_factor);
-            cs.SetFloat("r_sample_sq", (float) Math.Pow(r_sample, 2)*scale_factor);
-			var rot_mat = new float[,]{
-					{(float) Math.Cos((180-2*theta)*Math.PI/90), (float) -Math.Sin((180-2*theta)*Math.PI/90)},
-					{(float) Math.Sin((180-2*theta)*Math.PI/90), (float) Math.Cos((180-2*theta)*Math.PI/90)}
-				};
+            cs.SetBuffer(kernelHandle, "distancesOuter", outputBufferOuter);
+            cs.SetFloat("r_cell", r_cell);
+            cs.SetFloat("r_sample", r_sample);
+            cs.SetFloat("r_cell_sq", (float) Math.Pow(r_cell, 2));
+            cs.SetFloat("r_sample_sq", (float) Math.Pow(r_sample, 2));
+            var rot_mat = new float[,] {
+                {
+                    (float) Math.Cos((180 - 2 * theta) * Math.PI / 90),
+                    (float) -Math.Sin((180 - 2 * theta) * Math.PI / 90)
+                }, {
+                    (float) Math.Sin((180 - 2 * theta) * Math.PI / 90),
+                    (float) Math.Cos((180 - 2 * theta) * Math.PI / 90)
+                }
+            };
             //cs.SetMatrix("rot_mat", rot_mat);
-			
+
+
 
             /* Execute shader */
             inputBuffer.SetData(data);
-            cs.Dispatch(kernelHandle, size/2, 1, 1);
+            cs.Dispatch(kernelHandle, size, 1, 1);
             outputBufferInner.GetData(res);
             inputBuffer.Release();
             outputBufferOuter.Release();
-			outputBufferInner.Release();
+            outputBufferInner.Release();
+            
             
             /* Rescale results */
             var res_scaled = res.Select(v => v/scale_factor).ToArray();
-            //res = res_scaled; 
+            //res = res_scaled;
 
             //Debug.Log(String.Join("", Enumerable.Range(0,size-1).Select(i => res[i].ToString()).ToArray()));
             var strings = res.Select(v => v.ToString()).ToArray();
