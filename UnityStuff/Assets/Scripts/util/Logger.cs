5b8ea35ffc9db6b44e995a964ba0c9d181858c66
The Senate <iseve89+gitlab@gemail.com>
2020-06-14 17:40:35 +0200

    Updated Logger

--- UnityStuff/Assets/Scripts/util/Logger.cs
+++ UnityStuff/Assets/Scripts/util/Logger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
@@ -8,41 +9,108 @@ namespace util
 {
     public class Logger
     {
+        
+        public enum LogLevel : uint
+        {
+            All = 70,              // 1 + any event.
+            Verbose = 60,          // 2 + statements.
+            Info = 50,             // 4 + initializer method start- and end.
+            MethodLevel = 40,      // 5 + method start- and end.
+            Debug = 30,            // 3 + object creation- and destruction.
+            Warning = 20,          // 6 + warnings.
+            Error = 10,            // only errors.
+            None = 00
+        }
+
+        public enum EventType : uint
+        {
+            Error = 9, 
+            Warning = 19, 
+            Method = 29,                 // method start- or end. 
+            Class = 39,                  // constructor.
+            InitializerMethod = 49,      // every event inside initializer methods of class, unless events are declared as lower.
+            ShaderInteraction = 48,
+            Data = 59,                   // data retrieval, assignment, allocation, etc.
+            Step = 69,                    // a logical step or group of the code. 
+            Performance = 68
+        }
+        
         /**
          * Structure for simple log entries. Contains timestamp and message.
          * Can later be expanded with additional fields.
          */
         private readonly struct LogEntry
         {
-            public LogEntry(DateTime timestamp, string message)
+            public LogEntry(EventType eventType, DateTime timestamp, string message)
             {
-                this.timestamp = timestamp;
-                this.message = message;
+                Timestamp = timestamp;
+                Message = message;
+                EventType = eventType;
             }
 
-            public DateTime timestamp { get; }
-            public string message { get; }
+            private DateTime Timestamp { get; }
+            private string Message { get; }
+            public EventType EventType { get; }
 
-            public override string ToString() => $"{timestamp}:\t{message}";
+            public override string ToString()
+            {
+                return $"{Timestamp:HH:mm:ss.ffffff}:\t{Message}";
+            }
         }
         
         
         private readonly List<LogEntry> _logEntries;
         private string _defaultFolder = "Logs";
-        
+        private LogLevel PrintLevel { get; set; } = LogLevel.All;
+        private LogLevel WriteLevel { get; set; } = LogLevel.All;
+
+        public Logger SetPrintLevel(LogLevel logLevel)
+        {
+            PrintLevel = logLevel;
+            return this;
+        }
+
+        public Logger SetWriteLevel(LogLevel logLevel)
+        {
+            WriteLevel = logLevel;
+            return this;
+        }
+
         public Logger()
         {
             _logEntries = new List<LogEntry>();
         }
 
-        public void Log(string message)
+        public void Log(EventType eventType, string message, bool printDebug = false)
         {
-            _logEntries.Add(new LogEntry(DateTime.Now, message));
+            var entry = new LogEntry(eventType, DateTime.Now, message);
+            _logEntries.Add(entry);
+            if (printDebug || (int) eventType <= (int) PrintLevel) 
+                Debug.Log(entry.ToString());
         }
 
-        public void WriteToDebug()
+        private List<LogEntry> Restrict(LogLevel logLevel)
         {
-            _logEntries.ForEach(entry => Debug.Log(entry.ToString()));
+            return _logEntries
+                .Where(e => (int) e.EventType <= (int) logLevel)
+                .ToList();
+        }
+        
+        private List<LogEntry> Filter(List<EventType> eventTypes)
+        {
+            return _logEntries
+                .Where(e => eventTypes.Contains(e.EventType))
+                .ToList();
+        }
+
+        public void PrintToDebug(LogLevel logLevel)
+        {
+            Restrict(logLevel).ForEach(entry => Debug.Log(entry.ToString()));
+        }
+
+        public void PrintToDebug()
+        {
+            PrintToDebug(PrintLevel);
         }
 
         public void WriteToFile(string filePath)
---
+++ UnityStuff/Assets/Scripts/util/NullLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace util
+{
+    public class NullLogger : Logger
+    {
+        public NullLogger() {}
+
+        public new void Log(EventType eventType, string message, bool printDebug = false) {}
+
+        public new void PrintToDebug(LogLevel logLevel) {}
+        
+        public new void WriteToFile(string filePath) {}
+        
+        public new void AppendToFile(string filePath) {}
+    }
+}
