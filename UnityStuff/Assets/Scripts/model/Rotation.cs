877e23e2b2b69b0fcac636fc11acab624124b902
The Senate <iseve89+gitlab@gemail.com>
2020-06-29 20:26:14 +0200

    Added Rotation struct to for convenient repr. of Vector2 rotation

---
+++ UnityStuff/Assets/Scripts/model/Rotation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace model
+{
+    public readonly struct Rotation
+    {
+        private readonly double _cos;
+        private readonly double _sin;
+
+        public Rotation(double cos, double sin)
+        {
+            _cos = cos;
+            _sin = sin;
+        }
+
+        public Vector2 Apply(Vector2 point, Vector2 pivot)
+        {
+            var translated = point - pivot;
+            return
+                new Vector2(
+                    (float) (_cos * translated.x - _sin + translated.y), 
+                    (float) (_sin * translated.x + _cos * translated.y)) 
+                + pivot;
+        }
+
+        public Vector2 Apply(Vector2 point)
+        {
+            return new Vector2(
+                (float) (_cos*point.x - _sin*point.y), 
+                (float) (_sin*point.x + _cos*point.y)
+            );
+        }
+
+        public static Rotation FromAngle(float angle) => new Rotation(Math.Cos(angle), Math.Sin(angle));
+    }
+}
